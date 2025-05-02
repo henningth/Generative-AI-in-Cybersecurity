415548bb794b804341e3f75571fc84f74f7ae678
Henning Thomsen <henningthomsen@protonmail.com>
2025-05-02 07:59:00 +0200

    Slides and exercises for module 1

---
+++ 01_llm_basics.py
@@ -0,0 +1,28 @@
+"""
+Python script for exercise 1 in module 1, for the course Generative AI in Cybersecurity at UCN.
+
+Author: Henning Thomsen
+"""
+
+from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+# Be sure to have valid API keys in this file
+load_dotenv()
+
+# Define your prompt here, as per exercise text
+prompt = """
+Explain in max three sentences why one should study Generative AI in Cybersecurity.
+"""
+
+# Run OpenAI model
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+response = llm.invoke(prompt)
+print(response.content)
+
+# Run model via Groq
+llm = ChatGroq(model="llama3-8b-8192", temperature=0)
+response = llm.invoke(prompt)
+print(response.content)
